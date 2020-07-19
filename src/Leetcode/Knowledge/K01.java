a4819e57ed40f7ec8e8e195cec9bb9cb5714877e
Li-nsanity <15602023093@163.com>
2020-07-19 11:33:15 +0800

    2020.7.19

---
+++ src/Leetcode/Knowledge/K01.java
@@ -0,0 +1,12 @@
+package Leetcode.Knowledge;
+
+public class K01 {
+    public static void main(String[] args) {
+        Integer i1 = Integer.valueOf(100);
+        Integer i2 = 100;
+        Integer i3 = 200;
+        Integer i4 = 200;
+        System.out.println(i1 == i2);
+        System.out.println(i3 == i4);
+    }
+}
